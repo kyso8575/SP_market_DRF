633cb78b350071693579a54c9412e0572d6125ae
kyso8575 <kyso8575@gmail.com>
2024-12-26 11:21:28 +0900

    Add product edit and remove feature

--- products/urls.py
+++ products/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("product_list/", views.product_list, name="product_list"),
     path('like/<int:product_id>/', views.like_product, name='like_product'),
     path('product_detail/<int:product_id>', views.product_detail, name='product_detail'),
+    path('product_edit/<int:product_id>', views.product_edit, name='product_edit'),
+    path('delete/<int:product_id>/', views.product_delete, name='product_delete'),
 ]
