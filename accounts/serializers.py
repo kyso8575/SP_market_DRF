8b53eca65c3154f98c61fe5bb8fea14b301c9388
kyso8575 <kyso8575@gmail.com>
2024-12-26 19:12:59 +0900

    Edit serializers

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from products.models import Product
+from products.serializers import BaseProductSerializer
 
 User = get_user_model()
 
@@ -24,3 +24,36 @@ class FollowSerializer(serializers.ModelSerializer):
     def get_is_following(self, obj):
         request_user = self.context.get('request').user
         return request_user.following.filter(id=obj.id).exists()
+    
+
+
+# 🔹 Profile Product Serializer (사용자 상품 정보)
+class ProfileProductSerializer(BaseProductSerializer):
+    class Meta(BaseProductSerializer.Meta):
+        fields = BaseProductSerializer.Meta.fields + ['description']
+
+
+# 🔹 User Profile Serializer (사용자 정보 + 상품 목록)
+class UserProfileSerializer(serializers.ModelSerializer):
+    products = ProfileProductSerializer(many=True, read_only=True, source='product_set')
+    wishlist = BaseProductSerializer(many=True, read_only=True, source='liked_products')
+    profile_image = serializers.SerializerMethodField()
+    followers_count = serializers.IntegerField(source='followers.count', read_only=True)
+    following_count = serializers.IntegerField(source='following.count', read_only=True)
+    is_following = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = [
+            'id', 'username', 'email', 'first_name',
+            'profile_image', 'followers_count',
+            'following_count', 'is_following', 'products', 'wishlist'
+        ]
+
+    def get_profile_image(self, obj):
+        return obj.profile_image.url if obj.profile_image else None
+
+    def get_is_following(self, obj):
+        request_user = self.context.get('request').user
+        return request_user.following.filter(id=obj.id).exists()
+    
